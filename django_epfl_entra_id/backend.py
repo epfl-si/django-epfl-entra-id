902ed991dc674d9e1c2c663afe72cf848f8f0660
William Belle <william.belle@gmail.com>
2025-07-29 14:24:18 +0200

    [feature] Rewrite `EPFLOIDCAB` (#9)

--- django_epfl_entra_id/backend.py
+++ django_epfl_entra_id/backend.py
@@ -1,43 +1,71 @@
 import jwt
 from django.apps import apps
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
-User = get_user_model()
+OIDC_USER_MAPPING = (
+    ("username", "gaspar"),
+    ("email", "email"),
+    ("first_name", "given_name"),
+    ("last_name", "family_name"),
+)
 
 
 class EPFLOIDCAB(OIDCAuthenticationBackend):
+    def filter_users_by_claims(self, claims):
+        sciper = claims.get("uniqueid")
+        if not sciper:
+            return self.UserModel.objects.none()
+
+        is_app_using_profile = hasattr(settings, "AUTH_PROFILE_MODULE")
+
+        if is_app_using_profile:
+            user_profile_model = apps.get_model(
+                *settings.AUTH_PROFILE_MODULE.split(".")
+            )
+            try:
+                user_profile = user_profile_model.objects.filter(
+                    sciper=sciper
+                ).latest("id")
+                return self.UserModel.objects.filter(id=user_profile.user.id)
+            except user_profile_model.DoesNotExist:
+                return self.UserModel.objects.none()
+        else:
+            return self.UserModel.objects.filter(sciper=sciper)
+
     def create_user(self, claims):
-        user = self.update_attributes_if_same_sciper(claims)
-        if user:
-            return user
+        is_app_using_profile = hasattr(settings, "AUTH_PROFILE_MODULE")
 
-        user = User.objects.create_user(
-            username=claims.get("gaspar"),
-            email=claims.get("email"),
-            first_name=claims.get("given_name"),
-            last_name=claims.get("family_name"),
-        )
-
-        model_path = settings.USER_PROFILE_MODEL
-        ProfileModel = apps.get_model(*model_path.split("."))
-        profile, _ = ProfileModel.objects.get_or_create(user=user)
-        profile.sciper = claims.get("uniqueid")
-        profile.save()
+        if is_app_using_profile:
+            user = self.UserModel.objects.create_user(
+                username=claims.get("gaspar"),
+                email=claims.get("email"),
+                first_name=claims.get("given_name"),
+                last_name=claims.get("family_name"),
+            )
+            user_profile_model = apps.get_model(
+                *settings.AUTH_PROFILE_MODULE.split(".")
+            )
+            profile, _ = user_profile_model.objects.get_or_create(user=user)
+            profile.sciper = claims.get("uniqueid")
+            profile.save()
+        else:
+            user = self.UserModel.objects.create_user(
+                username=claims.get("gaspar"),
+                email=claims.get("email"),
+                first_name=claims.get("given_name"),
+                last_name=claims.get("family_name"),
+                sciper=claims.get("uniqueid"),
+            )
 
         return user
 
     def update_user(self, user, claims):
-        user.username = claims.get("gaspar")
-        user.email = claims.get("email")
-        user.first_name = claims.get("given_name")
-        user.last_name = claims.get("family_name")
+        for model_field, oidc_field in OIDC_USER_MAPPING:
+            if claims.get(oidc_field):
+                setattr(user, model_field, claims.get(oidc_field))
+
         user.save()
-
-        user.profile.sciper = claims.get("uniqueid")
-        user.profile.save()
-
         return user
 
     def get_userinfo(self, access_token, id_token, payload):
@@ -56,35 +84,3 @@ class EPFLOIDCAB(OIDCAuthenticationBackend):
         userinfo.update(id_token_decoded)
 
         return userinfo
-
-    def update_attributes_if_same_sciper(self, claims):
-        profile = self.get_user_profile(sciper=claims.get("uniqueid"))
-
-        print(profile)
-
-        if profile is None:
-            return None
-
-        updated = False
-        if profile.user.username != claims.get("gaspar"):
-            profile.user.username = claims.get("gaspar")
-            updated = True
-        if profile.user.email != claims.get("email"):
-            profile.user.email = claims.get("email")
-            updated = True
-        if profile.user.first_name != claims.get("given_name"):
-            profile.user.first_name = claims.get("given_name")
-            updated = True
-        if profile.user.last_name != claims.get("family_name"):
-            profile.user.last_name = claims.get("family_name")
-            updated = True
-
-        if updated:
-            profile.user.save()
-
-        return profile.user
-
-    def get_user_profile(self, sciper):
-        model_path = settings.USER_PROFILE_MODEL
-        ProfileModel = apps.get_model(*model_path.split("."))
-        return ProfileModel.objects.filter(sciper=sciper).first()
